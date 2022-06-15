e510876e010d5bbc8a8f720a0892330739159d26
ivaniraldi <iraldiban@gmail.com>
2022-06-15 10:13:19 -0300

    Deploy backend commit

--- api/src/app.js
+++ api/src/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
-require('./db.js');
+require('./db.js'); 
 
 const server = express();
 const cors = require('cors')
@@ -20,6 +20,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
