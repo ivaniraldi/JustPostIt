3d5779000300032cd429e615039c6d6c1ce9c978
ivaniraldi <iraldiban@gmail.com>
2022-06-17 01:50:40 -0300

    V2 Update 2 deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,8 +8,8 @@ import store from "../redux/store"
 import axios from 'axios'
 
 
-// axios.defaults.baseURL =  "https://justposthis.herokuapp.com/"
-axios.defaults.baseURL =  "http://localhost:3001/"
+axios.defaults.baseURL =  "https://justposthis.herokuapp.com/"
+// axios.defaults.baseURL =  "http://localhost:3001/"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
